c72edcad011d6c6a936f91846485530fb7aa5a23
avnisshsingh <avnisshsingh45@gmail.com>
2026-08-03 03:05:51 +0530

    03082026 fix 2

--- Code.js
+++ Code.js
@@ -10,6 +10,10 @@ function doGet() {
   return HtmlService
       .createTemplateFromFile("Index")
       .evaluate()
+      .addMetaTag(
+          "viewport",
+          "width=device-width, initial-scale=1, viewport-fit=cover"
+      )
       .setTitle("Smoky Swaad")
       .setXFrameOptionsMode(
           HtmlService.XFrameOptionsMode.ALLOWALL
